aee0cf9d002143470726b27f44b225729bcea285
Chandru <tochandru.engineer@gmail.com>
2022-09-03 01:13:32 +0530

    Stage One

---
+++ ken_app/app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from datetime import datetime
+
+
+class OrderModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=50)
+    customer_id = models.CharField(max_length=5)
+    phone_number = PhoneNumberField()
+    delivery_date = models.DateField() ## dd/mm/yyyy
+    delivery_time = models.TimeField(auto_now_add=False, blank=False) #'hh:mm'
+    order_date_time = models.DateTimeField(default = datetime.now(), blank = True)
+    address = models.CharField(max_length=100)
+    chicken_kg = models.CharField(max_length=10)
+    chicken_category = models.CharField(max_length=20)
+    chicken_citting = models.CharField(max_length=20)
+    mutton_kg = models.CharField(max_length=20)
+    mutton_category = models.CharField(max_length=20)
+    mutton_cutting = models.CharField(max_length=20)
+    status = models.BooleanField()
+    delivered = models.BooleanField()
+    amount = models.IntegerField()
+    review = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.customer_name
+
+    
+
+
+
---
+++ ken_app/app/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from .models import OrderModel
+
+def home(request):
+    return render(request, 'home.html')
+
+def order(request):
+    customer_id_data = 0
+    customer_name = 'chandru'
+    phone_number = '+91 9791394624'
+    address = 'Murugan Kovil Street, kakkoor, ramanathapuram'
+    if request.method =='POST':
+        chicken_kg = request.POST['quantity']
+        chicken_category = request.POST['category']
+        chicken_cutting = request.POST['cutting']
+        goat_kg = request.POST['quantity']
+        goat_category = request.POST['goat_category']
+        goat_cutting = request.POST['goat_cutting']
+        date = request.POST['date']
+        # print(chicken_kg)
+        # print(chicken_category)
+        # print(chicken_cutting)
+        # print()
+        # print(goat_kg)
+        # print(goat_category)
+        # print(goat_cutting)
+        # print(date)
+        object = OrderModel.objects.create(
+            customer_name = customer_name,
+            customer_id = customer_id_data,
+            phone_number = phone_number,
+            delivery_date = date,
+            delivery_time = '08:34',
+            address = address,
+            chicken_kg = chicken_kg,
+            chicken_category = chicken_category, 
+            chicken_citting = chicken_cutting,
+            mutton_kg = goat_kg,
+            mutton_category = goat_category,
+            mutton_cutting = goat_cutting,
+            status = False,
+            delivered = False,
+            amount = 100,
+            review = "")
+
+        object.save()
+        return redirect('test')
+    else:
+        return render(request, 'order.html')
+
+
+def test(request):
+    return render(request, 'navbar.html')
